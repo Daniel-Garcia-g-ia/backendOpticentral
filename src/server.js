2094dd1178616717e5c9e1ac83c85478495618e3
Daniel Garcia <daniel.g.1393@gmail.com>
2024-05-08 20:48:01 -0500

    [add] POST user Login

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const ENV = require ('../config/index');
+const cors = require ('cors');
 const db = require ('./db');
 const router = require ('../network/routes')
 const bodyParser = require ('body-parser')
+
+
+
 
 //mongodb+srv://danielg1393:Ff0itkw5fmPtboWQ@cluster0.5ftmkqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 db(`mongodb+srv://${ENV.config.db_user}:${ENV.config.db_pass}@cluster0.5ftmkqd.mongodb.net/${ENV.config.db_name}?retryWrites=true&w=majority&appName=Cluster0`)
@@ -10,6 +14,7 @@ db(`mongodb+srv://${ENV.config.db_user}:${ENV.config.db_pass}@cluster0.5ftmkqd.m
 
 
 const app = express();
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 
 router(app)
