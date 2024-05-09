cb2f486cf016e796477a5048866a150c5e3aaecb
Daniel Garcia <daniel.g.1393@gmail.com>
2024-05-08 20:05:49 -0500

    [add]setting at validation

--- src/components/login/network.js
+++ src/components/login/network.js
@@ -16,13 +16,19 @@ router.get('/login', function (req, res) {
 
 })
 router.post('/loginUser', function (req, res) {
-    console.log(req.body)
-    controller.loginUser(req, res)
-        .then((result) => {
-            return response.success(req, res, result, 200);
-        }).catch((e) => {
-            return response.error(req, res, 'Error al procesar la solicitud', 500, e.message)
-        })
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+        return response.error(req, res, "Error, el cuerpo de la solcitud está vacío", 500 )
+
+    } else {
+        controller.loginUser(req, res)
+            .then((result) => {
+                return response.success(req, res, result, 200);
+            }).catch((e) => {
+                return response.error(req, res, 'Error al procesar la solicitud', 500, e.message)
+            })
+    }
+
 
 })
 
