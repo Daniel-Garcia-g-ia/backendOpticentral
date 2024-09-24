0485dc3e563943ea5d70bca61701e8ecc571fc58
Daniel Garcia <daniel.g.1393@gmail.com>
2024-09-23 20:17:24 -0500

    [add]config vercel

--- src/server.js
+++ src/server.js
@@ -2,25 +2,27 @@ const express = require('express');
 const ENV = require ('../config/index');
 const cors = require ('cors');
 const db = require ('./db');
-const router = require ('../network/routes')
-const bodyParser = require ('body-parser')
+const router = require ('../network/routes');
+const bodyParser = require ('body-parser');
 
+// Función para inicializar el servidor
+function startServer() {
+    // Conectando a MongoDB
+    db(`mongodb+srv://${ENV.config.db_user}:${ENV.config.db_pass}@cluster0.5ftmkqd.mongodb.net/${ENV.config.db_name}?retryWrites=true&w=majority&appName=Cluster0`);
 
+    // Crear aplicación de Express
+    const app = express();
+    app.use(cors({ origin: '*' }));
+    app.use(bodyParser.json());
 
+    // Configurar las rutas
+    router(app);
 
-//mongodb+srv://danielg1393:Ff0itkw5fmPtboWQ@cluster0.5ftmkqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-db(`mongodb+srv://${ENV.config.db_user}:${ENV.config.db_pass}@cluster0.5ftmkqd.mongodb.net/${ENV.config.db_name}?retryWrites=true&w=majority&appName=Cluster0`)
+    // Iniciar el servidor
+    app.listen(ENV.config.port, () => {
+        console.log(`Servidor escuchando por el puerto ${ENV.config.port}`);
+    });
+}
 
-
-
-const app = express();
-app.use(cors({ origin: '*' }));
-app.use(bodyParser.json());
-
-router(app)
-
-
-
-app.listen(ENV.config.port, () => {
-    console.log(`Servidor escuchando por le puerto ${ENV.config.port}`);
-});
+// Exportar la función
+module.exports = startServer;
