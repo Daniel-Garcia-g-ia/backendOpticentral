f7dbbb03ea329f4da217914c7d50f6fa0e5bd51b
Daniel Garcia <daniel.g.1393@gmail.com>
2024-09-24 19:41:48 -0500

    [add]debug

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 // Función para inicializar el servidor
 
 // Conectando a MongoDB
-console.log(ENV.config.url)
+
 
 db(`mongodb+srv://${ENV.config.db_user}:${ENV.config.db_pass}@cluster0.5ftmkqd.mongodb.net/${ENV.config.db_name}?retryWrites=true&w=majority&appName=Cluster0`);
 
